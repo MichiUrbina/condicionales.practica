09acd32731356d8f767eddac4a786e80ee601c63
MichiUrbina <138550522+MichiUrbina@users.noreply.github.com>
2023-09-05 16:34:02 -0600

    practica condicionales

---
+++ condicionale.js
@@ -0,0 +1,9 @@
+const nombre = "Michel Urbina";
+let edad = 27;
+let nota = 98;
+
+if (nombre === "Michel Urbina" && nota >70 ){
+    console.log("Hola " + nombre + " su nota es =" + nota);
+} else {
+    console.log("Nombre no existe");
+}
